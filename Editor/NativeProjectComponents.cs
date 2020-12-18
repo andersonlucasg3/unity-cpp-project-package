028a54ac0ff02ed341f33d7e50647161a2426599
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2020-12-18 17:45:26 -0300

    Implemented logic to add the classes to the CMakeLists.txt project;

--- Editor/NativeProjectComponents.cs
+++ Editor/NativeProjectComponents.cs
@@ -14,69 +14,97 @@ namespace UnityCpp.Editor
         private const string _componentsFileName = "ComponentsEntryPoint";
         private static readonly string _gameSourcesPath = $"{_cppProjectPath}/UnityCppLib/Game"; 
         private static readonly string _componentsSourcePath = $"{_gameSourcesPath}/{_componentsFileName}.cpp";
-
-        private static TextWriter _sourceFileStream;
+        private static readonly string _cmakeListsFilePath = $"{_cppProjectPath}/CMakeLists.txt";
 
         [UsedImplicitly]
         [MenuItem(_generateRegisterComponentsMenuItem)]
         public static void GenerateNativeComponentsRegistration()
         {
+            string projectPath = Directory.GetParent(Application.dataPath).ToString();
+            
             Debug.Log("---->>> Beginning generation of components registration");
             
-            string projectPath = Directory.GetParent(Application.dataPath).ToString();
-            string gameSourcesPath = Path.Combine(projectPath, _gameSourcesPath);
+            WriteRegistrationSourceFile(projectPath, out string[] classesNames);
 
-            BeginSourceFile();
-            
-            string[] files = Directory.GetFiles(gameSourcesPath, "*.h");
-            IEnumerable<string> classesNames = from file in files 
-                select Path.GetFileName(file) 
-                into name where !name.Contains(_componentsFileName) 
-                select name.Replace(".h", "");
-            classesNames = classesNames.ToList();
-
-            foreach (string className in classesNames)
-            {
-                AddInclude(className);
-            }
-            
-            _sourceFileStream.WriteLine();
-            
-            _sourceFileStream.WriteLine("void RegisterComponents() {");
-            foreach (string className in classesNames)
-            {
-                AddRegisterCall(className);
-                
-                Debug.Log($"Registered class: {className}");
-            }
-            _sourceFileStream.WriteLine("}");
-            
-            _sourceFileStream.Close();
-            _sourceFileStream = null;
-            
             Debug.Log("---->>> Finished generating components registration");
+
+            Debug.Log("---->>> Beginning to update CMakeLists.txt project");
+
+            UpdateCMakeListsProject(projectPath, classesNames);
+            
+            Debug.Log("---->>> Finished updating CMakeLists.txt project");
         }
 
-        private static void BeginSourceFile()
+        private static void WriteRegistrationSourceFile(string projectPath, out string[] classesNames)
         {
+            void AddInclude(string headerName, TextWriter w)
+            {
+                w.WriteLine($"#include \"{headerName}.h\"");
+            }
+            
+            void AddRegisterCall(string className, TextWriter w)
+            {
+                w.WriteLine($"\t{className}::Register();");
+            }
+            
             if (File.Exists(_componentsSourcePath))
             {
                 File.Delete(_componentsSourcePath);
             }
+            
+            string gameSourcesPath = Path.Combine(projectPath, _gameSourcesPath);
 
-            _sourceFileStream = File.CreateText(_componentsSourcePath);
-            AddInclude(_componentsFileName);
-            _sourceFileStream.WriteLine();
+            TextWriter writer = File.CreateText(_componentsSourcePath);
+            AddInclude(_componentsFileName, writer);
+            
+            string[] files = Directory.GetFiles(gameSourcesPath, "*.h");
+            IEnumerable<string> names = from file in files 
+                select Path.GetFileName(file) 
+                into name where !name.Contains(_componentsFileName) 
+                select name.Replace(".h", "");
+            classesNames = names.ToArray();
+
+            writer.WriteLine();
+            
+            foreach (string className in classesNames)
+            {
+                AddInclude(className, writer);
+            }
+            
+            writer.WriteLine();
+            
+            writer.WriteLine("void RegisterComponents() {");
+            
+            foreach (string className in classesNames)
+            {
+                AddRegisterCall(className, writer);
+                
+                Debug.Log($"Registered class: {className}");
+            }
+            writer.WriteLine("}");
+            
+            writer.Close();
         }
 
-        private static void AddInclude(string headerName)
+        private static void UpdateCMakeListsProject(string projectPath, IReadOnlyList<string> classesNames)
         {
-            _sourceFileStream.WriteLine($"#include \"{headerName}.h\"");
-        }
+            string cmakeListsPath = Path.Combine(projectPath, _cmakeListsFilePath);
+            string cmakeListsContents = File.ReadAllText(cmakeListsPath);
+            
+            string[] outputNames = new string[classesNames.Count * 2];
+            
+            string classesPath = _gameSourcesPath.Replace($"{_cppProjectPath}/", "");
+            
+            for (int index = 0; index < outputNames.Length;)
+            {
+                outputNames[index] = $"{classesPath}/{classesNames[index]}.h";
+                index++;
+                outputNames[index] = $"{classesPath}/{classesNames[index]}.cpp";
+                index++;
+            }
 
-        private static void AddRegisterCall(string className)
-        {
-            _sourceFileStream.WriteLine($"\t{className}::Register();");
+            string outputCmakeLists = cmakeListsContents.Replace("#COMPONENTS_GO_HERE", string.Join("\n", outputNames));
+            File.WriteAllText(outputCmakeLists, cmakeListsPath);
         }
     }
 }
