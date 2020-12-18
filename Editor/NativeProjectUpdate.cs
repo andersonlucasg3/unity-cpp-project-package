1edf8e23e0f3fa27a30b9164aa0d6d18930bc41e
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2020-12-17 23:04:52 -0300

    Implementing a more informative update process;

--- Editor/NativeProjectUpdate.cs
+++ Editor/NativeProjectUpdate.cs
@@ -1,5 +1,8 @@
-﻿using UnityEditor;
+﻿using System.Threading;
+using UnityEditor;
 using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
 using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
 namespace UnityCpp.Editor
@@ -7,13 +10,34 @@ namespace UnityCpp.Editor
     public static class NativeProjectUpdate
     {
         private const string _updatePackageMenuItem = "Assets/UnityCpp/Update Package";
+        private const string _titleLabel = "Updating package";
+        private const string _infoLabel = "Unity Cpp Project";
 
         [MenuItem(_updatePackageMenuItem)]
         public static void UpdatePackage()
         {
+            float progress = 0F;
+            
             PackageInfo info = PackageInfo.FindForAssembly(typeof(NativeProjectUpdate).Assembly);
-            Client.Remove(info.packageId);
-            Client.Add(info.repository.url);
+            
+            RemoveRequest removeRequest = Client.Remove(info.packageId);
+            while (!removeRequest.IsCompleted)
+            {
+                EditorUtility.DisplayProgressBar(_titleLabel, _infoLabel, Mathf.Clamp(progress += 0.01F, 0F, .5F));
+                Thread.Sleep(100);
+            }
+            EditorUtility.DisplayProgressBar(_titleLabel, _infoLabel, .5F);
+            
+            AddRequest addRequest = Client.Add(info.packageId);
+            while (!addRequest.IsCompleted)
+            {
+                EditorUtility.DisplayProgressBar(_titleLabel, _infoLabel, Mathf.Clamp(progress += 0.01F, .5F, 1F));
+                Thread.Sleep(100);
+            }
+            EditorUtility.DisplayProgressBar(_titleLabel, _infoLabel, 1F);
+            Thread.Sleep(100);
+            
+            EditorUtility.ClearProgressBar();
         }
     }
 }
